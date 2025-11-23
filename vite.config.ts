999440b20ee07f14cf3d52f3b239fb0d5b35b068
lelamngochieu2811-creator <lelamngochieu2811@gmail.com>
2025-11-24 00:04:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Dòng này cực kỳ quan trọng để chạy trên GitHub Pages
+      // Tên repo của bạn là thung_lung_anh_sang_demo
+      base: '/thung_lung_anh_sang_demo/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
